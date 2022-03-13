b3efc36fb9eb5a452f73c49559648a033c3c7372
Mechalopa <92443696+Mechalopa@users.noreply.github.com>
2022-03-13 13:24:00 +0900

    Some items are no longer hidden from creative tags

--- src/main/java/net/mechalopa/hmag/item/ModItem.java
+++ src/main/java/net/mechalopa/hmag/item/ModItem.java
@@ -2,22 +2,15 @@ package net.mechalopa.hmag.item;
 
 import javax.annotation.Nullable;
 
-import net.mechalopa.hmag.util.ModTags;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.IRecipeType;
-import net.minecraft.tags.ITag;
 import net.minecraft.util.DamageSource;
-import net.minecraft.util.ResourceLocation;
 
 public class ModItem extends Item
 {
 	private final boolean isFoil;
 	private final ResistanceType resistanceType;
-	private final boolean isCreativeTabChecking;
-	@Nullable
-	private final ResourceLocation tagId;
 	private final int burnTime;
 
 	public ModItem(Item.Properties builder, ModItem.Properties builder1)
@@ -25,9 +18,7 @@ public class ModItem extends Item
 		super(builder);
 		this.isFoil = builder1.isFoil;
 		this.resistanceType = builder1.resistanceType;
-		this.tagId = builder1.tagId;
 		this.burnTime = builder1.burnTime;
-		this.isCreativeTabChecking = builder1.isCreativeTabChecking;
 	}
 
 	@Override
@@ -48,43 +39,10 @@ public class ModItem extends Item
 		return this.burnTime > 0 ? this.burnTime : -1;
 	}
 
-	@Override
-	protected boolean allowdedIn(ItemGroup group)
-	{
-		if (super.allowdedIn(group))
-		{
-			if (this.isCreativeTabChecking)
-			{
-				if (this.tagId != null)
-				{
-					ITag<Item> tag = ModTags.getItemTag(this.tagId);
-
-					if (tag != null && !tag.getValues().isEmpty())
-					{
-						return true;
-					}
-				}
-
-				return false;
-			}
-			else
-			{
-				return true;
-			}
-		}
-		else
-		{
-			return false;
-		}
-	}
-
 	public static class Properties
 	{
 		private boolean isFoil = false;
 		private ResistanceType resistanceType = ResistanceType.NORMAL;
-		private boolean isCreativeTabChecking = false;
-		@Nullable
-		private ResourceLocation tagId = null;
 		private int burnTime = -1;
 
 		public ModItem.Properties foil()
@@ -102,13 +60,6 @@ public class ModItem extends Item
 		public ModItem.Properties burnTime(int burnTimeIn)
 		{
 			this.burnTime = burnTimeIn;
-			return this;
-		}
-
-		public ModItem.Properties setCheckingItemTagId(ResourceLocation tagIdIn)
-		{
-			this.isCreativeTabChecking = true;
-			this.tagId = tagIdIn;
 			return this;
 		}
 	}
