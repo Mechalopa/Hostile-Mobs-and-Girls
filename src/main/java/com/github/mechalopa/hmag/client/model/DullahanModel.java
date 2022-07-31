7e5e4c90f2ec7186cf44609fb9b03a5e31336a9b
Mechalopa <92443696+Mechalopa@users.noreply.github.com>
2022-07-31 10:27:41 +0900

    Tweak model

--- src/main/java/com/github/mechalopa/hmag/client/model/DullahanModel.java
+++ src/main/java/com/github/mechalopa/hmag/client/model/DullahanModel.java
@@ -34,7 +34,7 @@ public class DullahanModel<T extends Mob> extends AbstractAdvancedGirlModel<T>
 
 	public static MeshDefinition createMesh(CubeDeformation cd)
 	{
-		MeshDefinition md = AbstractAdvancedGirlModel.createMesh(cd, 0.0F, 6);
+		MeshDefinition md = AbstractAdvancedGirlModel.createMesh(cd, 0.0F);
 		PartDefinition pd = md.getRoot();
 		PartDefinition bodypd = pd.getChild("body");
 		ModClientUtils.addC(bodypd, cd, "skirt_1", 0, 38, -3.5F, 0.0F, -2.0F, 7.0F, 1.0F, 4.0F, 0.0F, 11.0F, 0.0F);
