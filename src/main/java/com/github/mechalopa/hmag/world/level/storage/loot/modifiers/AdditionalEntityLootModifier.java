d1820cc12e870cec7a7438405c8c8b876d7823a8
Mechalopa <92443696+Mechalopa@users.noreply.github.com>
2023-05-07 16:06:26 +0900

    Fix terrible loot, close #16

--- src/main/java/com/github/mechalopa/hmag/world/level/storage/loot/modifiers/AdditionalEntityLootModifier.java
+++ src/main/java/com/github/mechalopa/hmag/world/level/storage/loot/modifiers/AdditionalEntityLootModifier.java
@@ -11,11 +11,14 @@ import com.google.gson.JsonObject;
 
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.GsonHelper;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.storage.loot.Deserializers;
 import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraft.world.level.storage.loot.functions.LootItemFunction;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
 import net.minecraftforge.common.loot.GlobalLootModifierSerializer;
 import net.minecraftforge.common.loot.LootModifier;
@@ -38,7 +41,9 @@ public class AdditionalEntityLootModifier extends LootModifier
 	@Override
 	public List<ItemStack> doApply(List<ItemStack> generatedLoot, LootContext context)
 	{
-		if (this.addition != null)
+		Entity entity = context.getParamOrNull(LootContextParams.THIS_ENTITY);
+
+		if (entity != null && entity instanceof LivingEntity && ((LivingEntity)entity).getLootTable() != null && context.getQueriedLootTableId().equals(((LivingEntity)entity).getLootTable()) && this.addition != null)
 		{
 			ItemStack stack = this.addition.getDefaultInstance();
 
