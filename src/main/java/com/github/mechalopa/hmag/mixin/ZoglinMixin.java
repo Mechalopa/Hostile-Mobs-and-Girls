fa2ccdf3a35d2dcdedfd63cd2f0fa8e62d05c6b8
Mechalopa <92443696+Mechalopa@users.noreply.github.com>
2025-06-20 22:07:49 +0900

    Add zoglin mixin

---
+++ src/main/java/com/github/mechalopa/hmag/mixin/ZoglinMixin.java
@@ -0,0 +1,24 @@
+package com.github.mechalopa.hmag.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import com.github.mechalopa.hmag.world.entity.CreeperGirlEntity;
+
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.monster.Zoglin;
+
+@Mixin(Zoglin.class)
+public abstract class ZoglinMixin
+{
+	@Inject(method = "isTargetable", at = @At(value = "HEAD"), cancellable = true)
+	public void hmag_isTargetable(LivingEntity livingentity, CallbackInfoReturnable<Boolean> cir)
+	{
+		if (livingentity != null && livingentity instanceof CreeperGirlEntity)
+		{
+			cir.setReturnValue(false);
+		}
+	}
+}
