fe85ac24079f9ccc8907eb98e37d770d37be65b8
Mechalopa <92443696+Mechalopa@users.noreply.github.com>
2025-06-22 00:17:54 +0900

    Nya

--- src/main/java/com/github/mechalopa/hmag/mixin/IronGolemMixin.java
+++ src/main/java/com/github/mechalopa/hmag/mixin/IronGolemMixin.java
@@ -1,6 +1,5 @@
 package com.github.mechalopa.hmag.mixin;
 
-import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,7 +10,6 @@ import com.github.mechalopa.hmag.registry.ModEntityTypes;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.animal.IronGolem;
 
-@Debug(export = true)
 @Mixin(IronGolem.class)
 public abstract class IronGolemMixin
 {
--- src/main/java/com/github/mechalopa/hmag/mixin/VillagerHostilesSensorMixin.java
+++ src/main/java/com/github/mechalopa/hmag/mixin/VillagerHostilesSensorMixin.java
@@ -15,7 +15,7 @@ import net.minecraft.world.entity.ai.sensing.VillagerHostilesSensor;
 @Mixin(VillagerHostilesSensor.class)
 public abstract class VillagerHostilesSensorMixin
 {
-	private static final ImmutableMap<EntityType<?>, Float> MOD_ACCEPTABLE_DISTANCE_FROM_HOSTILES = ImmutableMap.<EntityType<?>, Float>builder().put(ModEntityTypes.ALRAUNE.get(), 10.0F).put(ModEntityTypes.CRIMSON_SLAUGHTERER.get(), 10.0F).put(ModEntityTypes.DROWNED_GIRL.get(), 8.0F).put(ModEntityTypes.GIANT_MUMMY.get(), 8.0F).put(ModEntityTypes.HUSK_GIRL.get(), 8.0F).put(ModEntityTypes.JIANGSHI.get(), 8.0F).put(ModEntityTypes.NECROTIC_REAPER.get(), 8.0F).put(ModEntityTypes.OGRE.get(), 10.0F).put(ModEntityTypes.REDCAP.get(), 8.0F).put(ModEntityTypes.ZOMBIE_GIRL.get(), 8.0F).build();
+	private static final ImmutableMap<EntityType<?>, Float> MOD_ACCEPTABLE_DISTANCE_FROM_HOSTILES = ImmutableMap.<EntityType<?>, Float>builder().put(ModEntityTypes.ALRAUNE.get(), 10.0F).put(ModEntityTypes.CRIMSON_SLAUGHTERER.get(), 10.0F).put(ModEntityTypes.DROWNED_GIRL.get(), 8.0F).put(ModEntityTypes.GIANT_MUMMY.get(), 8.0F).put(ModEntityTypes.HUSK_GIRL.get(), 8.0F).put(ModEntityTypes.JIANGSHI.get(), 8.0F).put(ModEntityTypes.KASHA.get(), 4.0F).put(ModEntityTypes.NECROTIC_REAPER.get(), 8.0F).put(ModEntityTypes.OGRE.get(), 10.0F).put(ModEntityTypes.REDCAP.get(), 8.0F).put(ModEntityTypes.SAVAGEFANG.get(), 4.0F).put(ModEntityTypes.ZOMBIE_GIRL.get(), 8.0F).build();
 
 	@Inject(method = "Lnet/minecraft/world/entity/ai/sensing/VillagerHostilesSensor;isClose(Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/world/entity/LivingEntity;)Z", at = @At(value = "HEAD"), cancellable = true)
 	public void hmag_isClose(LivingEntity livingentity, LivingEntity livingentity1, CallbackInfoReturnable<Boolean> cir)
