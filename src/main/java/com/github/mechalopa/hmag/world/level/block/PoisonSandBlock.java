1898c20a1cb186bf3e7a872ef9687f282d8dd73f
Mechalopa <92443696+Mechalopa@users.noreply.github.com>
2025-11-30 11:22:58 +0900

    Waiwai

--- src/main/java/com/github/mechalopa/hmag/world/level/block/PoisonSandBlock.java
+++ src/main/java/com/github/mechalopa/hmag/world/level/block/PoisonSandBlock.java
@@ -20,14 +20,9 @@ public class PoisonSandBlock extends SoulSandBlock
 	@Override
 	public void entityInside(BlockState state, Level level, BlockPos pos, Entity entity)
 	{
-		if (!level.isClientSide && entity != null && entity instanceof LivingEntity)
+		if (!level.isClientSide() && entity != null && entity instanceof LivingEntity livingentity && !livingentity.hasEffect(MobEffects.POISON))
 		{
-			LivingEntity livingentity = (LivingEntity)entity;
-
-			if (!livingentity.hasEffect(MobEffects.POISON))
-			{
-				livingentity.addEffect(new MobEffectInstance(MobEffects.POISON, 5 * 20, 0));
-			}
+			livingentity.addEffect(new MobEffectInstance(MobEffects.POISON, 5 * 20, 0));
 		}
 	}
 }
